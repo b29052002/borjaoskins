85118beef22e094b3a58576bd28880b3ec6d8444
b29052002 <email.brunoalbuquerque@gmail.com>
2026-02-17 20:49:49 -0300

    Update env.js

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -1,17 +1,17 @@
-(function() {
-    const d = atob('aHR0cHM6Ly95eW95eGFubG9sb3Vwd29jemtoci5zdXBhYmFzZS5jb3xleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcGMzTWlPaUp6ZFhCaFltRnpaU0lzSW5KbFppSTZJbmw1YjNsNFlXNXNiMnh2ZFhCM2IyTjZhMmh5SWl3aWNtOXNaU0k2SW1GdWIyNGlMQ0pwWVhRaU9qRTNOamsyT0RNMk1qWXNJbVY0Y0NJNk1qQTROVEkxT1RZeU5uMC55VjlVc3p4WlcwRWU1WDZaajhPTG8xUV91UWZqOTlSSnZpYVpJSW1pTUFNfDY5NmVhNjJjNjkyZmIzYTM2ZjIzMDczMWY5N2MxNGJhNDAzNjhiMmJjYmU4ZWViMDJlNWRhYjdkNDFkYTk5ODR8QVBQX1VTUi00ZWQ2NjY0MC01MTFlLTRhZWEtODQ2Yi03MDQ5OWJlMTExNTY=').split('|');
-    window.ENV = {
-        get SUPABASE_URL() {
-            return d[0]
-        },
-        get SUPABASE_KEY() {
-            return d[1]
-        },
-        get ADMIN_HASH() {
-            return d[2]
-        },
-        get MP_PUBLIC_KEY() {
-            return d[3]
-        }
-    };
-})();
+exports.handler = async function(event, context) {
+  const e = Buffer.from([
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY,
+    process.env.ADMIN_HASH,
+    process.env.MP_PUBLIC_KEY
+  ].join('|')).toString('base64');
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/javascript',
+      'Cache-Control': 'no-store, no-cache, must-revalidate, private'
+    },
+    body: `(function(){const d=atob('${e}').split('|');window.ENV={get SUPABASE_URL(){return d[0]},get SUPABASE_KEY(){return d[1]},get ADMIN_HASH(){return d[2]},get MP_PUBLIC_KEY(){return d[3]}};setTimeout(()=>{delete window.ENV},10);})();`
+  };
+};
