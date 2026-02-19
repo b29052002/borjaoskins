4b801429fb991b71de2a50051e01724e9ce33fd1
b29052002 <email.brunoalbuquerque@gmail.com>
2026-02-19 13:30:06 -0300

    Update env.js

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -12,6 +12,6 @@ exports.handler = async function(event, context) {
       'Content-Type': 'application/javascript',
       'Cache-Control': 'no-store, no-cache, must-revalidate, private'
     },
-    body: `(function(){const d=atob('${e}').split('|');window.ENV={get SUPABASE_URL(){return d[0]},get SUPABASE_KEY(){return d[1]},get ADMIN_HASH(){return d[2]},get MP_PUBLIC_KEY(){return d[3]}};setTimeout(()=>{delete window.ENV},10);})();`
+    body: `(function(){const d=atob('${e}').split('|');window.ENV={get SUPABASE_URL(){return d[0]},get SUPABASE_KEY(){return d[1]},get ADMIN_HASH(){return d[2]},get MP_PUBLIC_KEY(){return d[3]}};})();`
   };
 };
